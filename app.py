edde34718955c59d63e8848a455e5aad66df5c40
Jason Blythe <jasonblythe@Jasons-MacBook-Pro.local>
2019-04-09 23:19:34 -0400

    changed back mongo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://mongodb:27017/')
+client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'],27017)
 db = client.tododb
 
 
